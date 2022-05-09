68d78e94d665ac0880814e99520ba7a0931e2732
Steve C Mathews <stevecmathews@Steves-MBP-2.lan>
2022-05-08 21:11:34 -0400

    final commit

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,9 @@
 const Marketplace = artifacts.require("Marketplace");
-const DappToken = artifacts.require("DappToken.sol");
+const InfinityToken = artifacts.require("InfinityToken.sol");
 module.exports = function(deployer) {
-  deployer.deploy(DappToken, 1000000).then(function() {
+  deployer.deploy(InfinityToken, 1000000).then(function() {
     // Token price is 0.001 Ether
     var tokenPrice = 1000000000000000;
-    return deployer.deploy(Marketplace, DappToken.address, tokenPrice);
+    return deployer.deploy(Marketplace, InfinityToken.address, tokenPrice);
   });
 };
